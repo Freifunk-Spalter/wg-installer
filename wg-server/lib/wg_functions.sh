d9b7b55f826664172ab7b2718169b83c797d5060
Nick Hainke <vincent@systemli.org>
2021-01-27 16:16:19 +0100

    update

--- wg-server/lib/wg_functions.sh
+++ wg-server/lib/wg_functions.sh
@@ -41,7 +41,7 @@ function wg_register {
 
 	# create wg tunnel
 	ip link add dev $ifname type wireguard
-	wg set $ifname listen-port $port private-key $gw_key peer $public_key allowed-ips 0.0.0.0/0
+	wg set $ifname listen-port $port private-key $gw_key peer $public_key allowed-ips ::0/0
 	ip -6 a a  $gw_ip dev $ifname
 	ip -6 a a fe80::1/64 dev $ifname
 	ip link set up dev $ifname
